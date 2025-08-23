b5eea0236538bc8bbea391c38d420e43775a6c0b
Jason_buaa <waterworth@qq.com>
2025-08-23 22:54:41 +0800

    Add click event listener for Insert button to trigger Unicode insertion

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -9,7 +9,8 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").Insert = insertUnicode;
+    document.getElementById("Insert").addEventListener("click", insertUnicode);
+
   }
 });
 
